4fc00e1e61a66c002200dbfe091a21095b738502
Michael Flanagan <mflanagan7@gmail.com>
2023-07-28 13:48:10 -0500

    fixing build errors

--- components/ArticleList.tsx
+++ components/ArticleList.tsx
@@ -2,7 +2,7 @@ import articleStyles from '../styles/Article.module.css'
 import ArticleItem from './ArticleItem'
 
 const ArticleList = ({ articles }: any) => {
-  // console.log(articles.data[0].title.replace(/ .*/,''))
+
   return (
     <>
       <h3 className='center'>Click the article card to visit the article source.</h3>
@@ -10,8 +10,8 @@ const ArticleList = ({ articles }: any) => {
         {articles.data?.map((article: any) => (
           <ArticleItem key={article.title.replace(/ .*/,'')} article={article} />
           ))
-          
         }
+      
       </div>
     </>
   )
