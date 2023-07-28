e15ee1c4d56db7f9638e8376b89209b84026c809
Michael Flanagan <mflanagan7@gmail.com>
2023-07-28 13:19:01 -0500

    fixing build errors

--- components/ArticleList.tsx
+++ components/ArticleList.tsx
@@ -7,7 +7,7 @@ const ArticleList = ({ articles }: any) => {
     <>
       <h3 className='center'>Click the article card to visit the article source.</h3>
       <div className={articleStyles.grid}> 
-        {articles && articles.data.map((article: any) => (
+        {articles.data?.map((article: any) => (
           <ArticleItem key={article.title.replace(/ .*/,'')} article={article} />
           ))
           
